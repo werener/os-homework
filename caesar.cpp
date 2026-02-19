297e60f6ce62cfd95b619a5cb8847ec59c3f2ef8
werener <lenyazhuk@yandex.ru>
2026-02-19 13:25:13 +0300

    feature: implemented xor encoding algorithm

---
+++ caesar.cpp
@@ -0,0 +1,13 @@
+static char KEY; 
+void caesar_enc(void *src_ptr, void *target_ptr, int len) {
+    char *src = (char*) src_ptr;
+    char *target = (char*) target_ptr;
+    for (int i = 0; i < len; ++i) {
+       char byte = src[i];
+       target[i] = byte ^ KEY;
+    }
+}
+
+void caesar_key(char key_) {
+    KEY = key_;
+}
---
+++ caesar.h
@@ -0,0 +1,2 @@
+void caesar_enc(void *src_ptr, void *target_ptr, int len);
+void caesar_key(char key_);
