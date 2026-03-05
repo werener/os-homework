f5bcabb50e757b1330a498ecd93a7a16300d29e3
werener <lenyazhuk@yandex.ru>
2026-03-05 23:25:57 +0300

    fix: remove segfault, caused by heap overflow

--- src/main.c
+++ src/main.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* (5) setup queue */
-    queue *q = create_queue(sizeof(chunk_t));
+    queue *q = create_queue(8);
     q->mutex = malloc(sizeof(pthread_mutex_t));
     pthread_mutex_init(q->mutex, NULL);
 
