362fa7dd7344e7a4e70d523fd5ef846cd76b62fb
werener <lenyazhuk@yandex.ru>
2026-05-27 12:07:35 +0300

    refactor: add missing frees. Confirm no leaking with valgrind

--- src/image_operations.c
+++ src/image_operations.c
@@ -23,7 +23,7 @@ void list(const char *img_path) {
 	}
 
 	FILE *img_f = fopen(img_path, "rb");
-	char **filenames = malloc(file_count * sizeof(char *));
+	char *filenames[file_count];
 	metadata_t metadata;
 	for (int i = 0; i < file_count; ++i) {
 		// read metadata of the file
@@ -40,7 +40,9 @@ void list(const char *img_path) {
 	for (int i = 0; i < file_count; ++i) {
 		if (strcmp(filenames[i], "\0"))
 			printf("- %s\n", filenames[i]);
+		free(filenames[i]);
 	}
+	fclose(img_f);
 }
 
 void get(const char *img_path, byte *key, const char *name_searched, const char *out) {
@@ -50,34 +52,38 @@ void get(const char *img_path, byte *key, const char *name_searched, const char 
 		return;
 	}
 	if (file_count == 0) {
-		fprintf(stderr, "Empty image\n");
+		printf("Empty image\n");
 		return;
 	}
+	
 
 	FILE *img_f = fopen(img_path, "rb");
 	metadata_t metadata;
+	int key_len = strlen((char *)key);
 	for (int i = 0; i < file_count; ++i) {
 		// read metadata of the file
 		fread(&metadata, sizeof(metadata_t), 1, img_f);
+
 		// check if the name matches
 		char *name_temp = malloc(metadata.name_len + 1);
 		fread(name_temp, metadata.name_len, 1, img_f);
-
 		if (strcasecmp(name_temp, name_searched) == 0) {
-			FILE *result_f = fopen(out, "wb");
-			if (!result_f) {
+
+			// try opening [out]
+			FILE *out_f = fopen(out, "wb");
+			if (!out_f) {
 				fprintf(stderr, "Cannot open %s", out);
 				free(name_temp);
 				return;
 			}
-
-			int key_len = strlen((char *)key);
+			
+			// concatenate salt and the provided [key]
 			byte *full_key = malloc(SALT_SIZE + key_len + 1);
 			memcpy(full_key, metadata.salt, SALT_SIZE);
 			memcpy(full_key + SALT_SIZE, key, key_len + 1);
 
+			
 			state_t *state = rc4_init(full_key);
-
 			size_t bytes_remained = metadata.data_len;
 			byte *buffer = malloc(BUFFER_SIZE);
 			while (bytes_remained > 0) {
@@ -88,17 +94,20 @@ void get(const char *img_path, byte *key, const char *name_searched, const char 
 				bytes_remained -= fread(buffer, 1, chunk_size, img_f);
 
 				rc4_apply(state, buffer, chunk_size);
-				fwrite(buffer, 1, chunk_size, result_f);
+				fwrite(buffer, 1, chunk_size, out_f);
 			}
 			free(buffer);
+			free(full_key);
 			free(name_temp);
 			free_state(state);
-			fclose(result_f);
+			fclose(out_f);
+			fclose(img_f);
 			return;
 		}
-		// free(name_temp);
+
 		// go to the next metadata block
 		fseek(img_f, metadata.data_len, SEEK_CUR);
+		free(name_temp);
 	}
 	fclose(img_f);
 }
